6f769fadae57f767994e26282cfdd47c927d3136
pintuSINGH2000 <94526600+pintuSINGH2000@users.noreply.github.com>
2022-12-03 23:42:11 +0530

    add input

---
+++ src/Component/Todoitem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todoitem(props){
+    return (
+        <div onClick={()=>
+            props.onChecked(props.id)
+            
+        }>
+            <li>{props.text}</li>
+        </div>
+    )
+}
+export default Todoitem;
